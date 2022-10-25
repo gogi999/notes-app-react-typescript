abeb949c2b25ca9e8b0011bfea07afc882b46ff3
gogi999 <g.petrovic9@gmail.com>
2022-10-25 19:24:08 +0200

    Notes App completed

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import './App.css';
+
+import React, { useState } from 'react';
+
+import {
+  Col,
+  Container,
+  Row,
+} from 'react-bootstrap';
+
+import CreateNotes from './components/CreateNotes';
+import Header from './components/Header';
+import NotesList from './components/NotesList';
+import { Note } from './models/note.model';
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([{
+    id: (new Date).toString(),
+    title: 'Meetings',
+    text: 'Schedule meeting with UI/UX team',
+    color: '#dfdfdf',
+    date: (new Date).toString()
+  }]);
+
+  return (
+    <>
+      <Header />
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <NotesList notes={notes} setNotes={setNotes} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CreateNotes notes={notes} setNotes={setNotes} />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default App;
